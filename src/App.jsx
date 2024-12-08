7dd4b01c8e36ea55ecf1280939cadd7be1fcfa7e
Michał Klepner <michal.klepner1@o2.pl>
2024-12-08 03:07:19 +0100

    Added AppLayout to route index

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppLayout from "./ui/AppLayout";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
       staleTime: 0,
     },
   },
 });
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<AppLayout />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,1 +1,7 @@
 import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return <div>Something</div>;
+};
+
+export default AppLayout;
